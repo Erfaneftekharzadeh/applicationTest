6cea769ef0465db7d27c2f35095cb751cccb6020
ErfanEftekharzadeh <el.rmco@gmail.com>
2020-04-11 23:52:39 +0200

    button and plain text added

--- app/src/main/java/com/erfan/android/applicationtest/MainActivity.java
+++ app/src/main/java/com/erfan/android/applicationtest/MainActivity.java
@@ -3,8 +3,23 @@ package com.erfan.android.applicationtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void clickFunction(View view){
+
+        /*EditText textView=(EditText) findViewById(R.id.TextField);*/
+
+        Log.i("information", "Button in the main page is pressed: ");
+
+        /*Log.i("user name is:", textView.getText().toString());*/
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
