3e360782428a4e57eb099161633d6aabdf8a8356
ErfanEftekharzadeh <el.rmco@gmail.com>
2020-04-16 00:04:55 +0200

    animate func used

--- app/src/main/java/com/erfan/android/applicationtest/MainActivity.java
+++ app/src/main/java/com/erfan/android/applicationtest/MainActivity.java
@@ -12,33 +12,20 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    public int counter = 2;
 
-    public void LoginButton(View view) {
+    public void fade(View view){
 
-        EditText username = findViewById(R.id.usernameInput);
-        EditText password = findViewById(R.id.passwordInput);
-        Log.i("username is", username.getText().toString());
-        Log.i("username is", password.getText().toString());
+        Log.i("fade function", "fade: pic was pressed");
 
-        Toast.makeText(this, "HI " + username.getText().toString(), Toast.LENGTH_LONG).show();
+        ImageView imageView= findViewById(R.id.picView);
 
-        ImageView pic = findViewById(R.id.picView);
-
-        if (counter == 2) {
-            pic.setImageResource(R.drawable.cat2);
-            counter = 1;
-            Log.i("counter", new Integer(counter).toString());
-        } else if (counter == 1) {
-            pic.setImageResource(R.drawable.cat1);
-            counter = 2;
-            Log.i("counter", new Integer(counter).toString());
-        }
-
-        Log.i("counterfinal", new Integer(counter).toString());
-
-
+        imageView.animate().alpha(0).setDuration(2000);
     }
+
+
+
+
+
 
 
     @Override
